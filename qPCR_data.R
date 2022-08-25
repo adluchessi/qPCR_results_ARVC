f2f9767ecf80fed290f7a4cb8366f8145c2f5dcd
Andre Luchessi <andre.luchessi@outlook.com>
2022-08-25 14:47:32 -0300

    Atualization

--- qPCR_data.R
+++ qPCR_data.R
@@ -1,69 +1,38 @@
-getwd()
-library(pheatmap)
 library(ggplot2)
 getwd()
-data<-read.csv("miRNA_iPSC-CM_BrS_qPCRresults.txt", sep = "\t", header = T)
-head(data)
+data<-read.csv("data_qPCR_ARVC_validation.txt", sep = "\t", header = T)
 
-data$Sample <- factor(data$Sample, levels=c("10_2991_CT","11_3002_CT","12_3014_CT",
-"14_3036_CT","7_2917_CT","9_2985_BrS","1_2747_BrS","13_2995_BrS","2_2748_BrS",
-"4_2775_BrS","5_2790_BrS","6_2914_BrS"))
-
-ggplot(data, aes(x=Sample, y=Cq.Mean, fill=Type)) +
+ggplot(data, aes(x=as.character(Sample), y=Cq.Mean, fill=Type)) +
   geom_boxplot(varwidth = TRUE, alpha=0.2) +
   theme(legend.position="none")
 
-samples1<- as.data.frame(t(data[c(1:24),3]))
-colnames(samples1)<- data[c(1:24),2]
-rownames(samples1)<- data[1,1]
+### Subset by miRNA for validation
+data_mir=subset(data, Target %in% c("celmiR393p", "hsamiR92a3p", "hsamiR165p", 
+                                    "hsamiR15a5p", "hsamiR1455p", "hsamiR19a3p", 
+                                    "hsamiR29a3p", "hsamiR5053p"))
+### Data transposition
+my_splits_samples <- split(data_mir, data_mir$Sample)
 
-samples2<- as.data.frame(t(data[c(25:48),3]))
-colnames(samples2)<- data[c(1:24),2]
-rownames(samples2)<- data[25,1]
+l <- list()
+for(i in 1:length(my_splits_samples)){
+  df.now <- as.data.frame(t(my_splits_samples[[i]]))
+  l[[i]] <- df.now
+}
 
-samples3<- as.data.frame(t(data[c(49:72),3]))
-colnames(samples3)<- data[c(1:24),2]
-rownames(samples3)<- data[49,1]
+l_2=list()
+l_3=list()
+for(i in 1:length(l)){
+  colnames(l[[i]])=l[[i]][2,]
+  df.now <- l[[i]][-c(1,2),]
+  l_2[[i]] <- df.now
+  df.now <- l_2[[i]][-c(2),]
+  l_3[[i]] <- df.now
+  rownames(l_3[[i]])<- paste('Sample',l[[i]][1,1],sep = "_")
+}
 
-samples4<- as.data.frame(t(data[c(73:96),3]))
-colnames(samples4)<- data[c(1:24),2]
-rownames(samples4)<- data[73,1]
+data_qPCR_ARVC=do.call(rbind,l_3)
 
-samples5<- as.data.frame(t(data[c(97:120),3]))
-colnames(samples5)<- data[c(1:24),2]
-rownames(samples5)<- data[97,1]
 
-samples6<- as.data.frame(t(data[c(121:144),3]))
-colnames(samples6)<- data[c(1:24),2]
-rownames(samples6)<- data[121,1]
-
-samples7<- as.data.frame(t(data[c(145:168),3]))
-colnames(samples7)<- data[c(1:24),2]
-rownames(samples7)<- data[145,1]
-
-samples8<- as.data.frame(t(data[c(169:192),3]))
-colnames(samples8)<- data[c(1:24),2]
-rownames(samples8)<- data[169,1]
-
-samples9<- as.data.frame(t(data[c(193:216),3]))
-colnames(samples9)<- data[c(1:24),2]
-rownames(samples9)<- data[193,1]
-
-samples10<- as.data.frame(t(data[c(217:240),3]))
-colnames(samples10)<- data[c(1:24),2]
-rownames(samples10)<- data[217,1]
-
-samples11<- as.data.frame(t(data[c(241:264),3]))
-colnames(samples11)<- data[c(1:24),2]
-rownames(samples11)<- data[241,1]
-
-samples12<- as.data.frame(t(data[c(265:288),3]))
-colnames(samples12)<- data[c(1:24),2]
-rownames(samples12)<- data[265,1]
-
-all_data<- rbind(samples1, samples2, samples3, samples4, samples5, samples6,
-                 samples7, samples8, samples9, samples10, samples11, samples12)
-dim(all_data)
 
 #FILT BY MIRNAs
 
